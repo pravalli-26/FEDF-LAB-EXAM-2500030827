9bbf7e6adbd28b2f5932be3d8261ff7266a61754
pravalli <2500030827@kluniversity.in>
2026-03-13 10:08:32 +0530

    project v1.0

---
+++ src/exam/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function NavBar() {
+  return (
+    <div>
+      <nav>
+        <link></link>
+        <link></link>
+        <link></link>
+        <link></link>
+      </nav>
+      <Routes>
+        <Route/>
+        <Route/>
+        <Route/>
+        <Route/>
+      </Routes>
+    </div>
+  )
+}
